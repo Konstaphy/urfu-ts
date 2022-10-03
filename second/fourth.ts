b6acc0c64209a303bb4aff224a4776897895a757
Konstantine <Konstaphy@gmail.com>
2022-10-03 22:18:58 +0500

    Задание 2.4

---
+++ second/fourth.ts
@@ -0,0 +1,9 @@
+function capitalizeFirstLettersAndRemoveIndexWord(sentence: string): string {
+    const wordArray = sentence.split(" ")
+    return wordArray
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .filter((_, index) => index !== wordArray[0].length)
+        .join(" ")
+}
+
+console.log(capitalizeFirstLettersAndRemoveIndexWord("Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."));
